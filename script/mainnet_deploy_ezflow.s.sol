ce9c29c17f0929ee940a7c36e1c4a20a4aceda79
denissergeev <denis@mevcapital.com>
2025-08-16 23:11:21 +0300

    chore: remove Amphor mentions and rename deployment script contracts

--- script/mainnet_deploy_ezflow.s.sol
+++ script/mainnet_deploy_ezflow.s.sol
@@ -10,7 +10,7 @@ import { UpgradeableBeacon } from
 import { BeaconProxy } from
     "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 
-contract MAINNET_DeployAmphor is Script {
+contract MAINNET_Deploy is Script {
     uint256 privateKey;
     uint16 fees;
     string vaultName;
@@ -78,8 +78,8 @@ contract MAINNET_DeployAmphor is Script {
         //console.log("Vault name: ",AsyncVault(implementation).symbol());
 
         // Mainnet 
-        // source .env && forge clean && forge script script/mainnet_deploy.s.sol:MAINNET_DeployAmphor --ffi --chain-id 1 --optimizer-runs 10000 --verifier-url ${VERIFIER_URL} --etherscan-api-key ${ETHERSCAN_API_KEY} --verify #--broadcast
+        // source .env && forge clean && forge script script/mainnet_deploy.s.sol:MAINNET_Deploy --ffi --chain-id 1 --optimizer-runs 10000 --verifier-url ${VERIFIER_URL} --etherscan-api-key ${ETHERSCAN_API_KEY} --verify #--broadcast
         // Sepolia
-        // source .env && forge clean && forge script script/mainnet_deploy.s.sol:MAINNET_DeployAmphor --ffi --chain-id 534351 --optimizer-runs 10000 --verifier-url ${VERIFIER_URL_SEPOLIA} --etherscan-api-key ${ETHERSCAN_API_KEY} --verify #--broadcast
+        // source .env && forge clean && forge script script/mainnet_deploy.s.sol:MAINNET_Deploy --ffi --chain-id 534351 --optimizer-runs 10000 --verifier-url ${VERIFIER_URL_SEPOLIA} --etherscan-api-key ${ETHERSCAN_API_KEY} --verify #--broadcast
     }
 }
